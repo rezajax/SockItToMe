137e76e458eee166a24aac923b65e632b4b359ab
Reza Jax <rezajaxg@gmail.com>
2025-01-11 14:06:55 +0400

    add module

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ tasks {
 
 
 dependencies {
-
     testImplementation(kotlin("test"))
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "SockItToMe"
-include("sock")
-include("socks")
+include("app")
